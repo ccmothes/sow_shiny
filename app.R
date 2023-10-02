4e9054df922b5ab49037e638e19d68680b662fcb
Caitlin Mothes <ccmothes@gmail.com>
2023-10-02 16:01:07 -0600

    bare bones of shiny app and .rmd generation

---
+++ app.R
@@ -0,0 +1,171 @@
+#
+# Scope of Work (SOW) internal tool to generate client info, budgets, and deliverable timelines
+#
+# Contributors: Caitlin Mothes (ccmothes) and Juan De La Torre (juandlt-csu)
+
+
+package_loader <- function(x) {
+  if (x %in% installed.packages()) {
+    library(x, character.only = TRUE)
+  } else {
+    install.packages(x)
+    library(x, character.only = TRUE)
+  }
+}
+
+lapply(c("shiny", "tidyverse", "googlesheets4"), package_loader)
+
+# connect to scope of work responses via google sheets
+
+## authenticate google sheets (see google_auth.R for set up)
+gs4_auth(cache=".secrets", email="ccmothes@gmail.com")
+
+## read in sheet as df
+sheet_url <- "https://docs.google.com/spreadsheets/d/1miAXjWnqgDg3wbi3Rp3NESF2fs7kTE7mQZNEP6qiOMA/edit#gid=2092154335"
+
+client_data <- gs4_get(sheet_url) %>%
+  read_sheet()
+
+
+
+# Define UI for application ----------
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("SOW Development Tool"),
+
+    # Sidebar with a slider input for number of bins 
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("client",
+                      "Select client:",
+                      choices = pull(client_data, "Last Name")),
+          # Print client name and org
+          h4("Client info"),
+          strong(textOutput("name")),
+          em(textOutput("org")),
+          textOutput("account"),
+          br(),
+          
+          # print project description
+          h4("Project Description"),
+          strong(textOutput("title")),
+          textOutput("description"),
+          br(),
+          
+          
+          #print tasks
+          h4("Tasks"),
+          textOutput("tasks"),
+          br(),
+          
+          # print deliverables
+          h4("Deliverables"),
+          textOutput("deliverables"),
+          br(),
+          
+          # print timeline
+          h4("Project Timeframe:"),
+          textOutput("timeframe"),
+          br(),
+          
+          # print funding
+          h4("Approximate funding available:"),
+          textOutput("funding"),
+          br(),
+          
+          # need a contract?
+          h4("Does this project require a contract?"),
+          textOutput("contract"),
+          br(),
+          
+          #print any other comments
+          h4("Any additional notes/comments:"),
+          textOutput("comments")
+          
+        ),
+        
+        mainPanel(
+          h2("Budget Calculation"),
+          h2("Deliverables Timeline"),
+          downloadButton("generate", "Generate SOW")
+        )
+        ))
+
+    
+)
+
+# Define server logic --------
+server <- function(input, output) {
+  
+  selected_cli <- reactive({
+    client_data %>% 
+      filter(`Last Name` == input$client)
+  })
+  
+  # print all client info to help user develop SOW ---------
+  output$name <- renderText(paste(selected_cli()$`First Name`, selected_cli()$`Last Name`))
+  output$org <- renderText(selected_cli()$`Organization, Department, or Affiliation`)
+  output$account <- renderText(selected_cli()$`Will financial support for this project come from a CSU account or external funds?`)
+  
+  output$title <- renderText(selected_cli()$`General Project Name`)
+  output$description <- renderText(selected_cli()$`General Project Description`)
+  
+  output$tasks <- renderText(selected_cli()$`Tasks to be performed by the Geospatial Centroid`)
+  
+  output$deliverables <- renderText({
+    if(is.na(selected_cli()$`DELIVERABLES (if different from above)`)){
+      "Tasks as stated above"
+    } else{
+      selected_cli()$`DELIVERABLES (if different from above)`
+    }
+             })
+  
+  output$timeframe <- renderText(selected_cli()$`Time frame:  When does this project need to be completed?`)
+  
+  output$funding <- renderText(selected_cli()$`Amount of funding available for this project, if known`)
+  
+  output$contract <- renderText({
+    if(is.na(selected_cli()$`If you are an off-campus entity, does your organization require a formal contract with CSU?`)){
+      "No"
+    } else {
+      selected_cli()$`If you are an off-campus entity, does your organization require a formal contract with CSU?`
+    }
+  })
+  
+  output$comments <- renderText(selected_cli()$`Other comments or information`)
+  
+  
+  # render .rmd to produce SOW --------------
+  output$generate <- downloadHandler(
+    filename = function() {
+      paste0(input$client, "_SOW_", Sys.Date(), ".docx")
+    },
+    content = function(file) {
+      # render file in temp directory so .knit files don't go in app directory
+      tempSOW <- file.path(tempdir(), "sow_template.Rmd")
+      tempImg <- file.path(tempdir(), "CSU-Signature.png")
+      tempRef <- file.path(tempdir(), "style_reference.docx")
+      file.copy("sow_template.Rmd", tempSOW, overwrite = TRUE)
+      file.copy("CSU-Signature.png", tempImg, overwrite = TRUE)
+      file.copy("style_reference.docx", tempRef, overwrite = TRUE)
+      rmarkdown::render(
+        tempSOW,
+        output_format = "word_document",
+        output_file = file,
+        params = list(
+          filtered_data = selected_cli()
+        ),
+        envir = new.env(parent = globalenv()),
+        clean = F,
+        encoding = "utf-8"
+      )
+    }
+  )
+  
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
